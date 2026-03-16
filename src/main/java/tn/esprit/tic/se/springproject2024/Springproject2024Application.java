887baee1b2165808318d5188b6ea24fe4ec27f3d
syrineklai8 <150437527+syrineklai8@users.noreply.github.com>
2026-03-16 11:34:42 +0100

    Added a comment for clarity

--- src/main/java/tn/esprit/tic/se/springproject2024/Springproject2024Application.java
+++ src/main/java/tn/esprit/tic/se/springproject2024/Springproject2024Application.java
@@ -1,13 +1,1 @@
-package tn.esprit.tic.se.springproject2024;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class Springproject2024Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Springproject2024Application.class, args);
-    }
-
-}
+// Added a harmless comment
